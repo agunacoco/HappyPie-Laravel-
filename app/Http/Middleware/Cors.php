b657cae317abb45c5257e6679fdcd6d5d21daf15
GU NA HYUN <80106663+agunacoco@users.noreply.github.com>
2021-11-28 12:11:29 +0900

    laravel proxt

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,8 +17,10 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
 
-        //ALLOW OPTIONS METHOD
-        return $next($request);
+        return $next($request)
+        ->header("Access-Control-Allow-Origin", "*")
+        ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
     
     }
 }
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer: {
         proxy: {
             // 프록시 요청을 보낼 api의 시작 부분
-            '/vi': {
+            '/v1': {
                 // 프록시 요청을 보낼 서버의 주소
                 target: 'https://kapi.kakao.com/v1',
                 changeOrigin: true,
