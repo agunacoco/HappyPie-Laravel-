80b95356cdbcc5dbc63b75a0da030aec057c85f4
GU NA HYUN <80106663+agunacoco@users.noreply.github.com>
2022-05-09 17:07:10 +0900

    fix - payment controller

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -10,20 +10,21 @@ use Illuminate\Http\Request;
 
 class PaymentsController extends Controller
 {
-    public function callpayment(Request $request){
+    public function callpayment(Request $request)
+    {
 
         $name = $request->itemsName;
         $http_host = "http://127.0.0.1:8000";
-        $adminkey = "2aec59941467f41efd86c2720cdb0c0b";
+        $adminkey = env("MIX_KAKAO_ADMIN_KEY");
         $cid = "TC0ONETIME";
         $headers = array(
-            'Authorization: KakaoAK '.$adminkey,
+            'Authorization: KakaoAK ' . $adminkey,
             'Content-type: application/x-www-form-urlencoded;charset=utf-8'
         );
 
-        $approval_url = $http_host."/happypies/payment/success";
-        $cancel_url = $http_host."/happypies/shoppingcart";
-        $fail_url = $http_host."/happypies/shoppingcart";
+        $approval_url = $http_host . "/happypies/payment/success";
+        $cancel_url = $http_host . "/happypies/shoppingcart";
+        $fail_url = $http_host . "/happypies/shoppingcart";
 
         $kakao_params = array(
             'cid' => $cid,
@@ -32,34 +33,35 @@ class PaymentsController extends Controller
             'item_name' => $name,
             'item_code' => $request->itemsId,
             'quantity' => $request->quantity,
-            'total_amount'=> $request->total_amount,
-            'tax_free_amount'=> 0,
-            'vat_amount'=>0,
-            'approval_url'=> $approval_url,
-            'cancel_url'=> $cancel_url,
-            'fail_url'=> $fail_url,
+            'total_amount' => $request->total_amount,
+            'tax_free_amount' => 0,
+            'vat_amount' => 0,
+            'approval_url' => $approval_url,
+            'cancel_url' => $cancel_url,
+            'fail_url' => $fail_url,
         );
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://kapi.kakao.com/v1/payment/ready");    
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);     
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);  
+        curl_setopt($ch, CURLOPT_URL, "https://kapi.kakao.com/v1/payment/ready");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_POSTFIELDS,  http_build_query($kakao_params));       //파라미터 값
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
-        curl_setopt($ch, CURLOPT_POST, true);  
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
         $output = curl_exec($ch);
         curl_close($ch);
         // dd(json_decode($output)->tid);
-        
+
         return response($output)->cookie('tid', json_decode($output)->tid, 1000, null, null, false, false);
     }
 
-    public function approve(Request $request){
+    public function approve(Request $request)
+    {
 
-        $adminkey = "2aec59941467f41efd86c2720cdb0c0b";
+        $adminkey = env("MIX_KAKAO_ADMIN_KEY");
         $cid = "TC0ONETIME";
         $headers = array(
-            'Authorization: KakaoAK '.$adminkey,
+            'Authorization: KakaoAK ' . $adminkey,
             'Content-type: application/x-www-form-urlencoded;charset=utf-8'
         );
 
@@ -72,25 +74,26 @@ class PaymentsController extends Controller
         );
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://kapi.kakao.com/v1/payment/approve");    
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);     
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);  
+        curl_setopt($ch, CURLOPT_URL, "https://kapi.kakao.com/v1/payment/approve");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_POSTFIELDS,  http_build_query($kakao_params));       //파라미터 값
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
-        curl_setopt($ch, CURLOPT_POST, true);  
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
         $output = curl_exec($ch);
         curl_close($ch);
-        
+
         return json_decode($output);
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
         $payment = Payment::create([
             'aid' => $request->aid,
             'user_id' => $request->partner_user_id,
             'order_id' => $request->partner_order_id,
-            'item_code'=> $request->item_code,
+            'item_code' => $request->item_code,
             'item_name' => $request->item_name,
             'approved_at' => $request->approved_at,
             'total' => $request->amount['total'],
@@ -98,14 +101,14 @@ class PaymentsController extends Controller
         ]);
 
         $userid = auth()->user()->id;
-        $menuIds =explode( ',',  $request->item_code );
+        $menuIds = explode(',',  $request->item_code);
         $payment->menus()->toggle($menuIds);
 
-        if($request->directPayment){
+        if ($request->directPayment) {
             Menu::find($request->item_code)->payments()->updateExistingPivot($payment->id, ['count' => $request->quantity]);
-        }else{
-            foreach($menuIds as $menuId){
-                $originalCount=User::find($userid)->menus()->where('menu_id',$menuId)->value('count');
+        } else {
+            foreach ($menuIds as $menuId) {
+                $originalCount = User::find($userid)->menus()->where('menu_id', $menuId)->value('count');
                 $cart = Menu::find($menuId)->payments()->updateExistingPivot($payment->id, ['count' => $originalCount]);
             }
             $user = User::find(auth()->user()->id);
@@ -113,35 +116,33 @@ class PaymentsController extends Controller
         }
 
         $delivery = Delivery::create([
-            'user_id'=> auth()->user()->id,
+            'user_id' => auth()->user()->id,
             'payment_id' => $payment->id,
-            'addr1'=> $request->addr1,
-            'addr2'=> $request->addr2,
-            'detailaddr'=> $request->zip,
-            'phone'=> $request->phoneNum,
-            'receiver'=>$request->receiver,
+            'addr1' => $request->addr1,
+            'addr2' => $request->addr2,
+            'detailaddr' => $request->zip,
+            'phone' => $request->phoneNum,
+            'receiver' => $request->receiver,
         ]);
 
-        
-       
+
+
         return $payment;
     }
 
-    public function showReceipt($order_id){
+    public function showReceipt($order_id)
+    {
 
         $payment = Payment::find($order_id);
 
-        return view('happypies.receipt', ['payment'=>$payment]);
+        return view('happypies.receipt', ['payment' => $payment]);
     }
 
-    public function index(){
+    public function index()
+    {
 
         $payments = Payment::where('user_id', auth()->user()->id)->with(['menus', 'delivery', 'user'])->latest()->paginate(3);
 
         return $payments;
     }
-
-
-
 }
-
